5bdb9cfcbb4943432cfe9f19a1f55f75c2300eb3
gift-nabaggala <nabaggalagift20@gmail.com>
2025-12-27 19:23:58 +0300

    Modified index.html page and index.js page

--- src/index.js
+++ src/index.js
@@ -17,12 +17,12 @@ function generatePoem(event){
       
       let context = "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem and separate each line with a <br/>. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem";
 
-      let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
+      let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
       let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
       let poemElement = document.querySelector("#poem");
       poemElement.classList.remove("hidden");
-      poemElement.innerHTML= `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+      poemElement.innerHTML= `<div class="generating">⏳ Generating a poem about ${instructionsInput.value}</div>`;
       
 
       //Make the API call
