f127f218184bb1c3e48b9e3c892d27b55377e943
gift-nabaggala <nabaggalagift20@gmail.com>
2025-12-27 17:12:31 +0300

    Added basic form handling and Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-alert("Hello World");
+function generatePoem(event){
+        event.preventDefault();
+
+       
+        new Typewriter("#poem", {
+          strings: "La tombe dit à la rose",
+          autoStart: true,
+          delay: 1,
+          cursor: "",
+        });
+
+
+}
+
+
+
+let poemFormElement = document.querySelector("#poem-form-generator");
+poemFormElement.addEventListener("submit", generatePoem);
+
+
